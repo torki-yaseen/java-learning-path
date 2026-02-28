250cc5e2f85996906f91dcb61292a1d1b01451f0
torki-yaseen <261757713+torki-yaseen@users.noreply.github.com>
2026-02-28 06:27:17 +0400

    File I/O logic to generate and write text files
    Enhance file writing with try-with-resources and custom text blocks

---
+++ WriteFile.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+    public static void main(String[] args) {
+        // program to write a simple txt file
+        // declare variables
+        String filePath = "C:\\Users\\Desktop\\test.txt";
+        String txt = """
+                 Next year I would created my own portfolio of app
+                 Next year I will be happier
+                 Next year i will be with my family
+                 Next year i will have ***k$
+                 After Allah will One day I will be
+                 Follow your heart desire
+                 Follow your dreams
+                 You don't know
+                 You will see
+                 Just if you believe
+                 Just believe and achieve
+                """;
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(txt);
+            System.out.println("File had create successfully!");
+        }catch (FileNotFoundException e){
+            System.out.println("File couldn't fount!");
+        }
+        catch (IOException e) {
+            System.out.println("Could not write a file!");
+        }
+    }
+}
