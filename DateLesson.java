6e6592591a0953c452589bfbb75a065d88b625af
torki-yaseen <261757713+torki-yaseen@users.noreply.github.com>
2026-03-04 22:56:12 +0400

    Impelentation of LocalDate class in java

---
+++ DateLesson.java
@@ -0,0 +1,40 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+public class DateLesson {
+    public static void main(String[] args) {
+
+        // use date
+        LocalDate date =  LocalDate.now();
+        System.out.println(date);
+        // use time
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+        // use both
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime);
+        // UTC
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        // format it
+
+        LocalDateTime dateTime1 = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("DD-MM-YYYY HH:MM:SS");
+        String newDateTime1 = dateTime1.format(formatter);
+        System.out.println(newDateTime1);
+        // place a date you can also compare
+        LocalDateTime datePast = LocalDateTime.of(2025,12,25,12,00,00);
+        LocalDateTime datePast2 = LocalDateTime.of(2021,11,25,12,10,00);
+        System.out.println(datePast);
+        System.out.println(datePast2);
+        if (datePast.isBefore(datePast2)) {
+            System.out.println("First date older than second");
+        }else {
+            System.out.println("Second older than the first");
+        }
+    }
+}
