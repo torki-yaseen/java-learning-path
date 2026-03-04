ff2d412d0973d088aaa9beca8cb17f64ebdd3086
torki-yaseen <261757713+torki-yaseen@users.noreply.github.com>
2026-03-04 22:26:00 +0400

    Refactor Hangman to load words from file
    Updated Hangman game to read words from a file and added game logic for guessing letters.

--- Hangman.java
+++ Hangman.java
@@ -1,10 +1,31 @@
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
         // Hangman game
-        String word = "Lemon";
+        File filePath = new File("src/word.txt");
+        ArrayList<String> words = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath)))
+        {
+            String line;
+            while ((line = reader.readLine()) != null){
+                words.add(line.trim());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+        } catch (IOException e) {
+            System.out.println("something went wrong");
+        }
+        Random random = new Random();
+        String word = words.get(random.nextInt(words.size()));
+
+        word = "lemon";
         ArrayList<Character> wordState = new ArrayList<>();
         int wrongGuesses =0;
         Scanner scanner = new Scanner(System.in);
@@ -15,11 +36,46 @@ public class Hangman {
         System.out.println("***********************");
         System.out.println("Hangman game with Java!");
         System.out.println("***********************");
-        System.out.print("Word: ");
-        for(char c : wordState){
-            System.out.print(c+ " ");
+        while (wrongGuesses <6){
+            System.out.println(hangmanArts(wrongGuesses));
+            System.out.print("Word: ");
+            for(char c : wordState){
+                System.out.print(c+ " ");
+            }
+            System.out.print("\n Guess a letter:");
+            char guess = scanner.next().toLowerCase().charAt(0);
+
+            // to know if the word is correct or not
+            if ((word.indexOf(guess)) >=0){
+                System.out.println("Correct guess");
+                for (int i =0 ; i<word.length();i++){
+                    // check in which index is the correct answer
+                    if (word.charAt(i) == guess){
+                        // update the wordState the ArrayList
+                        wordState.set(i,guess);
+                    }
+                }
+
+                    if (!wordState.contains('_')){
+                        System.out.println(hangmanArts(wrongGuesses));
+                        System.out.println("You win");
+                        System.out.println("Correct word was: " + word);
+                        break;
+                    }
+
+            }else {
+                System.out.println(" Wrong guess");
+                wrongGuesses ++;
+            }
+        }
+        if (wrongGuesses >=6){
+            System.out.println(hangmanArts(wrongGuesses));
+            System.out.println("Game over!");
+            System.out.println("Correct word was: " + word);
         }
         scanner.close();
+
+
     }
     static String hangmanArts(int wrongGuesses){
         return switch (wrongGuesses){
