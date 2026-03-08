a08f41fdeaf19a751aaa69d71770e2cd232a26f8
torki-yaseen <261757713+torki-yaseen@users.noreply.github.com>
2026-03-08 23:47:36 +0400

    HashMap for efficient key-value data management

---
+++ HashMapLesson.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class HashMapLesson {
+    public static void main(String[] args) {
+
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put("orange",3.99);
+        map.put("apple",5.99);
+        map.put("banana",1.99);
+        map.put("banana",0.99); // that will Override the first input
+        //map.remove("apple");// to remove it
+        System.out.println(map);
+        // to get the value of the key only
+        System.out.println(map.get("apple"));
+        // to check it there is a value of a key
+        System.out.println(map.containsKey("banana"));
+        // if the key had a certain value
+        System.out.println(map.containsValue(0.99));
+        // check the size
+        System.out.println("get the size of the map "+map.size());
+        // in practice
+        if (map.containsKey("watermelon")){
+            System.out.println(map.get("apple"));
+        }else {
+            System.out.println("key not found");
+        }
+        // to print all the prices ex. in the HashMap
+
+        for (String key : map.keySet()){
+            System.out.println(key + ": $"+ map.get(key));
+        }
+    }
+}
