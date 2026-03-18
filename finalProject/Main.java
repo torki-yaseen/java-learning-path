174d7f9b5cdbc339187f97cef2962afa5a88d9e7
torki-yaseen <261757713+torki-yaseen@users.noreply.github.com>
2026-03-18 23:57:32 +0400

    Final project

--- finalProject/Main.java
+++ finalProject/Main.java
@@ -2,21 +2,27 @@ package finalProject;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // java alarm
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime alarmTime;
+        LocalTime alarmTime =null;
 
-        System.out.print("Enter alarm time (HH:MM:SS) : ");
-        String inputTime = scanner.nextLine();
+        while(alarmTime == null){
+            try{
+                System.out.print("Enter in alarm time(HH:MM:SS) : ");
+                String inputTime = scanner.nextLine();
 
-        alarmTime = LocalTime.parse(inputTime,formatter);
-        System.out.println("Alarm set for : " + alarmTime);
-
-        scanner.close();
+                alarmTime= LocalTime.parse(inputTime,formatter);
+                System.out.println("Alarm set for " + alarmTime) ;
+                scanner.close();
+            }catch (DateTimeParseException e){
+                System.out.println("Enter valid input time! Use HH:MM:SS");
+            }
+        }
     }
 }
